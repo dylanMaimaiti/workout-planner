9c3956aae6c5c4aa2aee4ea9054a29c8b449a4ac
Dylan Maimaiti <dmaimait@uoguelph.ca>
2023-02-12 11:49:20 -0500

    Created homepage with all workout data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+
+//components
+import WorkoutDetails from "../components/WorkoutDetails";
 
 
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+
+    //fetch all workouts only once, when the component first renders
+    useEffect( () => {
+        const fetchWorkouts = async () => {
+            const response = await fetch("/api/workouts");
+            const data = await response.json();
+
+            if (response.ok) {
+                setWorkouts(data);
+            }
+        }
+
+        fetchWorkouts();
+    }, []);
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="worksout">
+                {workouts && workouts.map((workout) => {
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                })}
+            </div>
         </div>
     )
 };
