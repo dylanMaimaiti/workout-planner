3335c3655be6c075d418d491eff4a76dd71eae2e
Dylan Maimaiti <dmaimait@uoguelph.ca>
2023-02-12 12:14:27 -0500

    Added form for adding workouts and connected it to backend api

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 //components
 import WorkoutDetails from "../components/WorkoutDetails";
-
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
     const [workouts, setWorkouts] = useState(null);
@@ -21,13 +21,14 @@ const Home = () => {
         fetchWorkouts();
     }, []);
 
-    return (
+    return ( 
         <div className="home">
-            <div className="worksout">
+            <div className="workouts">
                 {workouts && workouts.map((workout) => {
-                    <WorkoutDetails key={workout._id} workout={workout} />
+                    return <WorkoutDetails key={workout._id} workout={workout} />
                 })}
             </div>
+            <WorkoutForm />
         </div>
     )
 };
